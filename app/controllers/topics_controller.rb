45b85bb94a0cf113e23691419784fb760ce7f92a
“Andrew <andrewwoods88@gmail.com>
2018-06-01 15:10:17 -0400

    created show action and show tests

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,11 +1,18 @@
 class TopicsController < ApplicationController
+	before_action :set_topic, only: [:show]
 
 	def index
 		@topics = Topic.all
 	end
 
 	def show
+		
+	end
 
+	private
+
+	def set_topic
+		@topic = Topic.friendly.find(params[:id])
 	end
 
 end
--- spec/features/topic_spec.rb
+++ spec/features/topic_spec.rb
@@ -26,10 +26,23 @@ describe 'navigate' do
 
   describe 'show' do
 
-    it 'can be reached sucesfully' do
+    before do 
       visit topic_path(@topic)
+    end
+
+    it 'can be reached sucesfully' do
       expect(page.status_code).to eq(200)
     end
+
+    it 'should display the topic title' do
+      expect(page).to have_css('h1', text: 'Sports')
+      # have_css matcher that not only looks for the content, 
+      # but also ensure it is inside of an HTML <h1> header tag
+    end
+
+    it 'should have a url that matches the custom url slug' do
+      expect(current_path).to have_content('sports')
+    end    
 
   end
 end
